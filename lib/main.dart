a3970696a16b157487dbab28e3602ac99766416f
hoetaekpro <hoetaek@student.snue.ac.kr>
2020-03-06 18:38:33 +0900

    swipable page view

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:focusplanner/pages/archive_page.dart';
+import 'package:focusplanner/pages/complete_page.dart';
+import 'package:focusplanner/pages/current_page.dart';
 
 void main() => runApp(MaterialApp(
       home: FocusPlanner(),
     ));
 
-class FocusPlanner extends StatelessWidget {
+class FocusPlanner extends StatefulWidget {
+  @override
+  _FocusPlannerState createState() => _FocusPlannerState();
+}
+
+class _FocusPlannerState extends State<FocusPlanner> {
+  int _page = 1;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: _page,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Focus Planner'),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (newPage) {
+          setState(() {
+            _page = newPage;
+          });
+        },
+        children: <Widget>[
+          ArchivePage(),
+          CurrentPage(),
+          CompletePage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        onTap: (index) {
+          this._pageController.animateToPage(index,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut);
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.archive), title: Text("목록")),
+          BottomNavigationBarItem(icon: Icon(Icons.work), title: Text("작업")),
+          BottomNavigationBarItem(icon: Icon(Icons.check), title: Text("완료")),
+        ],
+      ),
     );
   }
 }
