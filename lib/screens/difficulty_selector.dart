c8983c58f777113450f5a67e865936299c79db85
hoetaekpro <hoetaek@student.snue.ac.kr>
2020-03-07 03:54:52 +0900

    difficulty description

--- lib/screens/difficulty_selector.dart
+++ lib/screens/difficulty_selector.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class DifficultySelector extends StatelessWidget {
   final ValueChanged<int> onDifficultyChanged;
   final int currentDifficulty;
+  final diffSentences = [
+    '굉장히 쉽고 단기간에 해결가능합니다.',
+    '단기간에 해결가능합니다.',
+    '해결하는데 시간이 걸립니다.',
+    '어렵습니다.',
+    '오래 걸리고 어렵습니다.',
+  ];
 
   DifficultySelector(
       {@required this.currentDifficulty, @required this.onDifficultyChanged});
@@ -12,25 +19,33 @@ class DifficultySelector extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 20.0),
       child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15.0),
-            border: Border.all(
-              width: 1.0,
-              color: Theme.of(context).primaryColor,
-            )),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: List<Widget>.generate(
-              5,
-              (int index) => DifficultyLevel(
-                  level: index + 1,
-                  isActive: currentDifficulty == index + 1,
-                  onTap: () {
-                    onDifficultyChanged(index + 1);
-                  })),
-        ),
-      ),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15.0),
+              border: Border.all(
+                width: 1.0,
+                color: Theme.of(context).primaryColor,
+              )),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: List<Widget>.generate(
+                    5,
+                    (int index) => DifficultyLevel(
+                        level: index + 1,
+                        isActive: currentDifficulty == index + 1,
+                        onTap: () {
+                          onDifficultyChanged(index + 1);
+                        })),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, top: 5.0, bottom: 10.0),
+                child: Text('${diffSentences[currentDifficulty - 1]}'),
+              )
+            ],
+          )),
     );
   }
 }
