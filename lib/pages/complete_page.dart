cbfb68ab6d896f93dfdc1a2129177104b69fadf7
hoetaekpro <hoetaek@student.snue.ac.kr>
2020-03-31 19:15:54 +0900

    added calendar view for complete goals

--- lib/pages/complete_page.dart
+++ lib/pages/complete_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:focusplanner/constants.dart';
 import 'package:focusplanner/models/goal.dart';
+import 'package:focusplanner/screens/complete_calendar.dart';
 import 'package:focusplanner/widgets/column_builder.dart';
 import 'package:focusplanner/widgets/date_list_tile.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-class CompletePage extends StatelessWidget {
+enum CompleteMode {
+  List,
+  Calendar,
+}
+
+class CompletePage extends StatefulWidget {
+  @override
+  _CompletePageState createState() => _CompletePageState();
+}
+
+class _CompletePageState extends State<CompletePage> {
+  CompleteMode _completeMode = CompleteMode.List;
+  Map<DateTime, List<Goal>> _events = Map<DateTime, List<Goal>>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,31 +29,47 @@ class CompletePage extends StatelessWidget {
       ),
       body: ValueListenableBuilder(
           valueListenable: Hive.box(Boxes.goalBox).listenable(),
-          builder: (context, Box box, widget) {
-            return SingleChildScrollView(
-              child: Column(
-                children: getDateGoalsColumn(box),
-              ),
-            );
+          builder: (context, Box goalBox, widget) {
+            if (_completeMode == CompleteMode.List) {
+              return _buildDateGoalsColumn(goalBox);
+            } else
+              return Container(
+                child: CompleteCalendar(
+                  events: _events,
+                ),
+              );
           }),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.swap_horiz),
+        onPressed: () {
+          setState(() {
+            _completeMode = _completeMode == CompleteMode.List
+                ? CompleteMode.Calendar
+                : CompleteMode.List;
+          });
+        },
+      ),
     );
   }
 
-  List<Widget> getDateGoalsColumn(Box goalBox) {
+  Widget _buildDateGoalsColumn(Box goalBox) {
     List<Widget> _children = [];
+
     List<DateTime> uniqueGoalDateList;
     uniqueGoalDateList = goalBox.values
-        .where((goal) => (goal as Goal).date != null)
-        .map((goal) => (goal as Goal).getDay())
+        .cast<Goal>()
+        .where((goal) => goal.date != null)
+        .map((goal) => goal.getDay())
         .toSet()
         .toList();
-    print(uniqueGoalDateList);
     uniqueGoalDateList.sort((b, a) => a.compareTo(b));
     uniqueGoalDateList.forEach((date) {
       List<Goal> goalsOnDate = goalBox.values
-          .where((goal) => (goal as Goal).isOnDate(date))
           .cast<Goal>()
+          .where((goal) => goal.isOnDate(date))
           .toList();
+
+      _events.putIfAbsent(date, () => goalsOnDate);
 
       if (goalsOnDate.length > 0) _children.add(DateListTile(date: date));
       _children.add(ColumnBuilder(
@@ -56,6 +86,10 @@ class CompletePage extends StatelessWidget {
         },
       ));
     });
-    return _children;
+    return SingleChildScrollView(
+      child: Column(
+        children: _children,
+      ),
+    );
   }
 }
